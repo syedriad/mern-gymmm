417bcb54bfbecd492ff2080607d69d244f3e8803
Muhammad Riad <140616498+syedriad@users.noreply.github.com>
2026-01-13 15:42:55 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { WorkoutContextProvider } from './context/workoutContext.jsx'
+import { WorkoutContextProvider } from './context/WorkoutContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
